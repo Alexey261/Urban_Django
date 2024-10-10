40738994e3f205c3344a7adb2f3829c591235e89
Алексей <dlyx@yandex.ru>
2024-10-10 13:00:19 +0300

    add files

---
+++ example1/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse('Hello!')
+
+
+# class index2(TemplateView):
+#     template_name = 'index2.html'
